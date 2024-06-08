bbf7c1a94d48738da2ea52bb8dba03b92978aa62
RusBigLeba <wjy_2103113@sjtu.edu.cn>
2024-06-08 11:37:43 +0800

    import_test

---
+++ import_test.py
@@ -0,0 +1,6 @@
+# A import test to see whether the robot is exported well from SolidWorks
+
+import pybullet
+
+robot_file_path = "asset\Assembly_overall_v0.0.SLDASM\urdf\Assembly_overall_v0.0.SLDASM.urdf"
+
